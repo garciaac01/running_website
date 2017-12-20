574cdbda5da4126629a836a2720a185175b5aa15
garciaac01 <garciaac01@gmail.com>
2017-12-20 17:33:02 -0500

    Added base html and pages for all and individual runners.

--- running_site/urls.py
+++ running_site/urls.py
@@ -7,4 +7,8 @@ from . import views
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
+    # List of all runners
+    url(r'^runners/$', views.runners, name='runners'),
+    # Individual runner page
+    url(r'^runners/(?P<runner_id>\d+)/$', views.runner, name='runner'),
 ]
--- running_site/views.py
+++ running_site/views.py
@@ -1,12 +1,33 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# Django imports
 from django.shortcuts import render
 from django.http import HttpResponse
+
+# My imports
+from .models import Runner
 
 # Create your views here.
 def index(request):
     """
     Home page test
     """
-    return HttpResponse("This is a test")
+    return render(request, 'running_site/index.html')
+
+def runners(request):
+    """
+    Example of a page that would display all the runners in the database.
+    """
+    runners = Runner.objects.order_by('first_name')
+    context = {'runners': runners}
+    return render(request, 'running_site/runners.html', context)
+
+def runner(request, runner_id):
+    """
+    Individual runner page.
+    """
+    runner = Runner.objects.get(id=runner_id)
+    performances = runner.performance_set.all()
+    context = {'runner': runner, 'performances': performances}
+    return render(request, 'running_site/runner.html', context)
