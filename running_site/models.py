e5abc6f53d2a22bad7a26c57bd83a48ac4061bce
garciaac01 <garciaac01@gmail.com>
2017-12-21 20:59:46 -0500

    Updates to models and unicode strings.

--- running_site/models.py
+++ running_site/models.py
@@ -34,20 +34,60 @@ class Performance(models.Model):
     """
     runner = models.ForeignKey(Runner)
     meet = models.ForeignKey(Meet)
-    number_distance = models.IntegerField()
+    number_distance = models.DecimalField(max_digits=8, decimal_places=1, validators=[MinValueValidator(0)])
     UNIT_CHOICES = (
         ('m', 'm'),
         ('km', 'km'),
         ('mi', 'mi')
     )
     units = models.CharField(choices=UNIT_CHOICES, default='km', max_length=255)
-    hours = models.IntegerField(null=True, blank=True)
-    minutes = models.IntegerField()
-    seconds = models.DecimalField(decimal_places=2, max_digits=4, validators=[MaxValueValidator(60.0), MinValueValidator(00)])
+    hours = models.IntegerField(null=True, blank=True,
+                                validators=[MinValueValidator(0)])
+    minutes = models.IntegerField(validators=[MaxValueValidator(60), MinValueValidator(0)])
+    seconds = models.DecimalField(decimal_places=2, max_digits=4,
+                                  validators=[MaxValueValidator(60.0), MinValueValidator(00)])
 
     def __unicode__(self):
         """Return a string representation of the performance."""
         if self.hours:
-            return str(self.hours) + ":" + str(self.minutes) + ":" + str(self.seconds)
+            hour = str(self.hours)
+            minute = ""
+            second = ""
+            if len(str(self.minutes)) < 2:
+                minute = str(self.minutes)
+                while len(minute) < 2:
+                    minute = "0" + minute
+            else:
+                minute = str(self.minutes)
+            
+            secs = str(self.seconds).split('.')
+            if len(secs[0]) < 2:
+                second = secs[0]
+                while len(second) < 2:
+                    second = "0" + second
+            else:
+                second = secs[0]
+            
+            if len(secs) != 1 and len(secs[1]) != 0 and secs[1] != "00":
+                second += "." + secs[1]
+    
+            #return str(self.hours) + ":" + str(self.minutes) + ":" + str(self.seconds)
+            return hour + ":" + minute + ":" + second
         else:
-            return str(self.minutes) + ":" + str(self.seconds)
+            minute = ""
+            second = ""
+            minute = str(self.minutes)
+            
+            secs = str(self.seconds).split('.')
+            if len(secs[0]) < 2:
+                second = secs[0]
+                while len(second) < 2:
+                    second = "0" + second
+            else:
+                second = secs[0]
+            
+            if secs[1] != "" and secs[1] != "00":
+                second += "." + secs[1]
+        
+            #return str(self.hours) + ":" + str(self.minutes) + ":" + str(self.seconds)
+            return minute + ":" + second
